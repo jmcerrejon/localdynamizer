f40d3f491c04cbe7432931a481289e6b75a3771d
Jose Cerrejon <ulysess@gmail.com>
2020-08-09 12:26:24 +0200

    📦 NEW: Location Mi/S/Mo and relationship

---
+++ database/migrations/2020_08_09_082127_create_location_user_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('location_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('location_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('location_user');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // TODO To theirs own seed file
+        DB::table('locations')->insert([
+            'name' => 'Isla Cristina',
+            'postal_code' => '21400',
+            'slug' => 'isla-cristina',
+        ]);
+        
         DB::table('users')->insert([
             'name' => 'Jose Cerrejon',
             'email' => 'ulysess@gmail.com',
@@ -19,6 +26,12 @@ class DatabaseSeeder extends Seeder
             'password' => '$2y$10$7HzofNKxFCjjdvyFdJk9TOUOWDlefF31no6oSKM3gPb8EiOg2N126', // secret
 			'created_at' => new DateTime(),
 			'updated_at' => new DateTime(),
+        ]);
+
+        // TODO Just for quick test. Change with their respective IDs 
+        DB::table('location_user')->insert([
+            'user_id' => 1,
+            'location_id' => 1,
         ]);
     }
 }
