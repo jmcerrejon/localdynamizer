f79154e09d0a683bd26ae5db5a1d7d3b0991c149
Jose Cerrejon <ulysess@gmail.com>
2020-08-10 18:44:51 +0200

    👌 IMPROVE: Add method delFile

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Get the image with slug format
+     *
+     * @param  String  $path
+     * @return void
+     */
+	public function delFile($path)
+	{
+		if (Storage::disk('public')->exists($path)) {
+			return Storage::disk('public')->delete($path);
+		}
+
+		return false;
+	}
 }
