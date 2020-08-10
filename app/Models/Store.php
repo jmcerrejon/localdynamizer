e9cb6a48e0b2a04e39da62e022dcf82bd054ffca
Jose Cerrejon <ulysess@gmail.com>
2020-08-10 13:56:37 +0200

    👌 IMPROVE

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -6,8 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Store extends Model
 {
+    protected $casts = [
+		'created_at' => 'datetime:d/m/Y H:i',
+		'updated_at' => 'datetime:d/m/Y H:i',
+		'is_active'	 => 'boolean'
+    ];
+
+    protected $fillable = [
+        'name', 'active', 'url', 'img_file'
+    ];
+
+	protected $dates = [
+		'created_at', 'updated_at'
+	];
+
     public function user()
     {
         return $this->belongsTo(\App\Models\User::class);
     }
+
+    public function actives()
+	{
+		return $this->where('is_active', 1)->get();
+	}
+
+	public function setActiveAttribute($value)
+	{
+		$this->attributes['active'] = ($value === "on") ? 1 : 0;
+	}
 }
