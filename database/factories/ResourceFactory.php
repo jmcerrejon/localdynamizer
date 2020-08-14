440c597b559c968066dd1649d1ccdd29fd890aa5
Jose Cerrejon <ulysess@gmail.com>
2020-08-14 12:54:10 +0200

    👌 IMPROVE: Randomize user_id

--- database/factories/ResourceFactory.php
+++ database/factories/ResourceFactory.php
@@ -3,15 +3,17 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\Mime;
+use App\Models\User;
 use App\Models\Hashtag;
 use App\Models\Resource;
 use Faker\Generator as Faker;
 
 $factory->define(Resource::class, function (Faker $faker) {
+    $userIds = User::get()->modelKeys();
     $mimeIds = Mime::get()->modelKeys();
 
     return [
-        'user_id' => 1,
+        'user_id' => $userIds[array_rand($userIds, 1)],
         'mime_id' => $mimeIds[array_rand($mimeIds, 1)],
         'body' => $faker->paragraph,
         'path' => $faker->imageUrl(1024, 768),
