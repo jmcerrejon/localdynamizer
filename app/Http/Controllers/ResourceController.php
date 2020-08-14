eed1ae83f08a7b45dce538584545449661062856
Jose Cerrejon <ulysess@gmail.com>
2020-08-14 09:48:47 +0200

    👌 IMPROVE: store resource and save hashtags (basic) on store method

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Exception;
 use App\Models\Mime;
+use App\Models\Hashtag;
 use App\Models\Resource;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\ResourceRequest;
@@ -49,7 +51,10 @@ class ResourceController extends Controller
         $validated = $request->validated();
         $hashtags = $validated['hashtags'];     
         $validated['user_id'] = Auth::user()->id;
-        // $validated['path'] = $request->file('resource_file'); // TODO Handle resize if image, etc...
+        if ($request->has('resource_file')) {
+            $validated['path'] = $this->saveFile($request->file('resource_file')); // TODO Handle resize if image, etc...
+        }
+        
         unset($validated['hashtags'], $validated['resource_file']);
 
         try {
@@ -58,7 +63,7 @@ class ResourceController extends Controller
             return back()->withError($e->getMessage())->withInput();
         }
 
-        // TODO Save hashtags
+        $this->saveHashtags($hashtags);
 
         return redirect()->route('recursos.index')->with('message', 'Recurso guardado.');
     }
@@ -106,5 +111,40 @@ class ResourceController extends Controller
     public function destroy($id)
     {
         dd('destroy');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Object  $file
+     * @return  String
+     */
+    private function saveFile($file)
+    {
+        $extension = $file->getClientOriginalExtension();
+        $mime = $file->getMimeType();
+        $signature = time();
+        $tmpFileName = Str::slug(str_replace(".$extension", '', $file->getClientOriginalName()));
+        $tmpFileName .= "_{$signature}";
+
+        $fileName = strtolower("{$tmpFileName}.{$extension}");
+
+        return $file->storeAs("public/resources/$mime", $fileName);
+    }
+
+    /**
+     * Get an array and transform each elements into hashtags
+     *
+     * @param  String  $hashtags
+     * @return  void
+     */
+    private function saveHashtags($hashtags)
+    {
+        $tagsNames = explode(',', str_replace(' ','', $hashtags));
+
+        foreach($tagsNames as $tagName){
+            $normalizedHashtag = strtolower(($tagName[0] != '#') ? '#'.$tagName : $tagName);
+            Hashtag::firstOrCreate(['name' => $normalizedHashtag])->save();
+        }
     }
 }
