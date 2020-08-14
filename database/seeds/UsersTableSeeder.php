b792fba343cede5ef2b726702954a45f163eed62
Jose Cerrejon <ulysess@gmail.com>
2020-08-14 12:54:33 +0200

    👌 IMPROVE: Add new user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,10 +15,15 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'Jose Cerrejon',
             'email' => 'ulysess@gmail.com',
-			'email_verified_at' => new DateTime(),
+            'email_verified_at' => new DateTime(),
             'password' => '$2y$10$7HzofNKxFCjjdvyFdJk9TOUOWDlefF31no6oSKM3gPb8EiOg2N126', // secret
-			'created_at' => new DateTime(),
-			'updated_at' => new DateTime(),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Jose Cardenas',
+            'email' => 'ceo@dinamizadorsocial.com',
+            'email_verified_at' => new DateTime(),
+            'password' => '$2y$10$7HzofNKxFCjjdvyFdJk9TOUOWDlefF31no6oSKM3gPb8EiOg2N126', // secret
         ]);
     }
 }
