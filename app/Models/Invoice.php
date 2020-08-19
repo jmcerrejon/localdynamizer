caf1f8a973327b29479b5b4f8220f46ba3a9ecbf
Jose Cerrejon <ulysess@gmail.com>
2020-08-19 09:34:36 +0200

    👌 IMPROVE: Relationships

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Invoice extends Model
 {
     public function invoiceitems() : HasMany
     {
         return $this->HasMany(\App\Models\Invoiceitem::class);
+    }
+
+    public function store() : BelongsTo
+    {
+        return $this->belongsTo(\App\Models\Store::class);
     }
 }
--- app/Models/Store.php
+++ app/Models/Store.php
@@ -3,48 +3,54 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Store extends Model
 {
     protected $casts = [
-		'created_at' => 'datetime:d/m/Y H:i',
-		'updated_at' => 'datetime:d/m/Y H:i',
+        'created_at' => 'datetime:d/m/Y H:i',
+        'updated_at' => 'datetime:d/m/Y H:i',
     ];
 
     protected $appends = [
-		'img_path'
-	];
+        'img_path'
+    ];
 
     protected $fillable = [
         'user_id', 'payment_method_id', 'comercial_name', 'business_name', 'cif', 'is_active', 'contact_name', 'address', 'locality', 'population', 'postal_code', 'email', 'public_phone', 'contact_phone', 'whatsapp', 'website', 'subscription_type', 'logo_path'
     ];
 
-	protected $dates = [
-		'created_at', 'updated_at'
+    protected $dates = [
+        'created_at', 'updated_at'
     ];
     
     public function getImgPathAttribute()
-	{
+    {
         if (strpos($this->logo_path, 'https') !== false) {
             return $this->logo_path;
         }
 
-		return config('app.url').'storage/'.$this->logo_path;
-	}
+        return config('app.url').'storage/'.$this->logo_path;
+    }
 
     public function actives()
-	{
-		return $this->where('is_active', 1)->get();
-	}
-
-	public function setIsActiveAttribute($value)
-	{
-		$this->attributes['is_active'] = ($value === "on") ? 1 : 0;
+    {
+        return $this->where('is_active', 1)->get();
     }
-    
+
+    public function setIsActiveAttribute($value)
+    {
+        $this->attributes['is_active'] = ($value === "on") ? 1 : 0;
+    }
+
     public function user() : BelongsTo
     {
         return $this->belongsTo(\App\Models\User::class);
+    }
+
+    public function invoices() : HasMany
+    {
+        return $this->HasMany(\App\Models\Invoice::class);
     }
 }
