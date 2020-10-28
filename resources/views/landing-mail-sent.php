2eab1cf3846f7e9707428492eb233e250cabca7e
Jose Cerrejon <ulysess@gmail.com>
2020-10-28 20:04:09 +0100

    👌 IMPROVE: view landing sent mail

---
+++ resources/views/landing-mail-sent.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dinamizador Local</title>
+    <meta property="og:title" content="Dinamizador Local" />
+    <meta name="twitter:title" content="Dinamizador Local" />
+    <meta name="description" content="Gana dinero promocionando en internet a las empresas de tu ciudad" />
+    <meta property="og:description" content="Gana dinero promocionando en internet a las empresas de tu ciudad" />
+    <meta name="twitter:description" content="Gana dinero promocionando en internet a las empresas de tu ciudad" />
+    <meta name="og:site_name" content="Dinamizador Local" />
+    <meta name="og:locale" content="es_ES" />
+    <meta name="copyright" content="Llega Digital" />
+    <meta name="author" content="Jose Manuel Cerrejon Gonzalez" />
+    <link rel="stylesheet" href="./css/main-landing.css">
+</head>
+<body class="flex items-center justify-center h-screen">
+    <div class="p-4 text-center">
+        <img class="w-1/2 p-4 mx-auto" src="images/landing/banner-header-local-dynamizer.svg" alt="people market mobile art">
+        <p class="text-2xl text-bold">¡Datos del formulario recibido!.</p>
+        <p>Nos pondremos muy pronto en contacto con usted.</p>
+    </div>
+</body>
+<script>
+    setTimeout(function() {
+        window.location="https://dinamizadorlocal.com";
+    }, 4000);
+</script>
+</html>
