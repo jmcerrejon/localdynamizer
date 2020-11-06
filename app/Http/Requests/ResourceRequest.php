62290eab6460c81855402223fc699017d3e9ba30
cerrejon <ulysess@gmail.com>
2020-11-06 13:33:23 +0100

    👌 IMPROVE: Add attributes

--- app/Http/Requests/ResourceRequest.php
+++ app/Http/Requests/ResourceRequest.php
@@ -6,21 +6,11 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ResourceRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
@@ -28,6 +18,15 @@ class ResourceRequest extends FormRequest
             'body' => ['required', 'max:200'],
             'hashtags' => ['required'], // TODO Transform "first, second" on array of hashtags here: ['#first', '#second']
             'resource_file' => ['nullable', 'file'], // TODO If mime_id is != 1, 'required'
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'mime_id' => 'tipo de recurso',
+            'body' => 'mensaje/descripción',
+            'resource_file' => 'recurso multimedia',
         ];
     }
 }
