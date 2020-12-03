8b7900cdb19f9fa24ee8250509654e874795aad4
Jose Cerrejon <ulysess@gmail.com>
2020-12-03 18:05:10 +0100

    👌 IMPROVE: logout route

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -89,7 +89,7 @@
                         <hr>
 
                         <!-- item -->
-                        <form method="POST" action="http://comm.test/logout">
+                        <form method="GET" action="{{ route('admin.logout') }}">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <a class="px-4 py-2 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 hover:text-gray-900 transition-all duration-300 ease-in-out"
                             href="logout" onclick="event.preventDefault(); this.closest('form').submit();">
