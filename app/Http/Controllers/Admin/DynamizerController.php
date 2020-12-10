09f9be2a0be5219400848f058d19dff4ff8cf5cb
Jose Cerrejon <ulysess@gmail.com>
2020-12-10 19:27:09 +0100

    👌 IMPROVE: Index, edit. Add setDataTableHeading & setDataTableRows

--- app/Http/Controllers/Admin/DynamizerController.php
+++ app/Http/Controllers/Admin/DynamizerController.php
@@ -2,19 +2,22 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Carbon\Carbon;
 use App\Models\User;
 use Illuminate\View\View;
-use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Redirect;
+use Illuminate\Database\Eloquent\Collection;
 
 class DynamizerController extends Controller
 {
     public function index(User $users): View
     {
-        return view('admin.dynamizers.index', compact('users'));
+        $headings = $this->setDataTableHeading();
+        $data = $this->setDataTableRows($users);
+
+        return view('admin.dynamizers.index', compact('headings', 'data'));
     }
 
     public function create()
@@ -31,7 +34,7 @@ class DynamizerController extends Controller
 
     public function edit(User $dynamizer): View
     {
-        return view('', compact('dynamizer'));
+        return view('admin.dynamizers.edit', compact('dynamizer'));
     }
 
     public function update(UserRequest $request, $id): RedirectResponse
@@ -50,5 +53,35 @@ class DynamizerController extends Controller
         $dynamizer->delete();
 
         return redirect()->route('dynamizers.index');
+    }
+
+    private function setDataTableHeading(): array
+    {
+        return [
+            'ID', 'Nombre', '¿ Activo ?', 'Último acceso', 'Acciones'
+        ];
+    }
+
+    private function setDataTableRows($users): Collection
+    {
+        return $users
+            ->select('id', 'name', 'is_enabled', 'last_login_at')
+            ->get()
+            ->map(function ($item) {
+                $item->is_enabled = $item->is_enabled ? 'SI' : 'NO';
+                $item->last_login_at = Carbon::parse($item->last_login_at)->format("d/m/Y H:i");
+                $item->actions = '
+                <div class="flex">
+                    <button type="button" class="btn ml-2" onclick="editItem('.$item->id.');" title="Editar">
+                        <i class="fa fa-pen"></i>
+                    </button>
+                    <button type="button" class="btn btn-danger ml-2" onclick="deleteItem('.$item->id.', \''.$item->name.'\');" title="Eliminar">
+                        <i class="fa fa-trash"></i>
+                    </button>
+                </div>
+                ';
+
+                return $item;
+            });
     }
 }
