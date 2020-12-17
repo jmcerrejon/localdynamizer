2423c2960b462df9273ef38719b4d2505379855a
Jose Cerrejon <ulysess@gmail.com>
2020-12-17 11:18:29 +0100

    👌 IMPROVE: Remove Esc key onkeydown

--- public/admin/js/modal.js
+++ public/admin/js/modal.js
@@ -6,19 +6,6 @@ for (var i = 0; i < closemodal.length; i++) {
     closemodal[i].addEventListener('click', toggleModal);
 }
 
-document.onkeydown = function(evt) {
-    evt = evt || window.event;
-    var isEscape = false;
-    if ('key' in evt) {
-        isEscape = evt.key === 'Escape' || evt.key === 'Esc';
-    } else {
-        isEscape = evt.keyCode === 27;
-    }
-    if (isEscape && document.body.classList.contains('modal-active')) {
-        toggleModal();
-    }
-};
-
 function toggleModal() {
     const body = document.getElementById('container');
     const modal = document.querySelector('.modal');
