aa255a7867eb755e13ea749035a8babdd5a100a5
Jose Cerrejon <ulysess@gmail.com>
2021-01-07 16:49:30 +0100

    👌 IMPROVE: activities API REST

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('locations', API\LocationController::class);
 Route::get('categories', API\CategoryController::class);
+Route::get('activities', API\ActivityController::class);
 Route::get('stores/{postal_code}', API\StoreController::class);
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
