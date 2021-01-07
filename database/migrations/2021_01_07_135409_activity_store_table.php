e1a3f09519cca2fb9975741d315a569a4a746ca5
Jose Cerrejon <ulysess@gmail.com>
2021-01-07 16:17:30 +0100

    📦 NEW: pivot table

---
+++ database/migrations/2021_01_07_135409_activity_store_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ActivityStoreTable extends Migration
+{
+    public function up() : void
+    {
+        Schema::create('activities_store', function (Blueprint $table) {
+            $table->foreignId('activity_id')->constrained()->onDelete('cascade');
+            $table->foreignId('store_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    public function down() : void
+    {
+        Schema::dropIfExists('activities_store');
+    }
+}
