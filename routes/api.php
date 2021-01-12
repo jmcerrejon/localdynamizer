299fc7bf8cbe0ba6a3a0408361bd0ade46dd7e8b
Jose Cerrejon <ulysess@gmail.com>
2021-01-12 17:02:45 +0100

    👌 IMPROVE: route

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('locations', API\LocationController::class);
 Route::get('categories', API\CategoryController::class);
 Route::get('activities', API\ActivityController::class);
-Route::get('stores/{postal_code}', API\StoreController::class);
+Route::get('stores/location/{id}', API\StoreController::class);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
