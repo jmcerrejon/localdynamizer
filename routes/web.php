38cccedd8426c884ac79d9d7a1b0c86b58f1547f
Jose Cerrejon <ulysess@gmail.com>
2021-02-12 12:40:24 +0100

    👌 IMPROVE: Refactor code

--- routes/web.php
+++ routes/web.php
@@ -2,35 +2,40 @@
 
 use Illuminate\Support\Facades\{Auth, Route};
 
+// Landing
+
 Route::view('/', 'landing');
-
 Route::post('submit-landing-form', SubmitLandingFormController::class)->name('submit-landing-form');
-
 Route::get('get_location', GetLocationController::class)->name('get-location');
+
+// Auth routes
 
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::get('home', [ App\Http\Controllers\HomeController::class, 'index' ])->name('home.index');
-    Route::get('home/search', [ App\Http\Controllers\HomeController::class, 'search' ])->name('home.search');
+
+    // Home
+
+    Route::get('/home', [ App\Http\Controllers\HomeController::class, 'index' ])->name('home.index');
+    Route::get('/home/search', [ App\Http\Controllers\HomeController::class, 'search' ])->name('home.search');
     Route::get('ficheros', FileManagerController::class)->name('filemanager');
 
-    Route::get('recursos/hashtags', [ App\Http\Controllers\ResourceController::class, 'filterByHashtags'])->name('recursos.hashtags.search');
+    // Resources
 
-    Route::get('stores/datatables', [ App\Http\Controllers\StoreController::class, 'anyData' ])->name('stores.datatables');
+    Route::group(['preffix' => 'recursos'], function () {
+        Route::get('/hashtags', [ App\Http\Controllers\ResourceController::class, 'filterByHashtags'])->name('recursos.hashtags.search');
+        Route::get('/descarga/{id}', [ App\Http\Controllers\ResourceController::class, 'download'])->name('recursos.download')->where('id', '[0-9]+');
+    });
 
-    Route::get('invoices/datatables', [ App\Http\Controllers\InvoiceController::class, 'anyData'])->name('invoices.datatables');
+    Route::get('/stores/datatables', [ App\Http\Controllers\StoreController::class, 'anyData' ])->name('stores.datatables');
+    Route::get('/invoices/datatables', [ App\Http\Controllers\InvoiceController::class, 'anyData'])->name('invoices.datatables');
 
-    Route::get('recursos/descarga/{id}', [ App\Http\Controllers\ResourceController::class, 'download'])
-        ->name('recursos.download')
-        ->where('id', '[0-9]+');
+    // Stores
 
-    Route::get('establecimientos/{id}/horarios', [ App\Http\Controllers\StoreController::class, 'showOpening'])
-    ->name('establecimientos.show-opening')
-    ->where('id', '[0-9]+');
-
-    Route::post('establecimientos/{id}/horarios', [ App\Http\Controllers\StoreController::class, 'saveOpening'])
-    ->name('establecimientos.save-opening');
+    Route::group(['preffix' => 'establecimientos'], function () {
+        Route::get('/{id}/horarios', [ App\Http\Controllers\StoreController::class, 'showOpening'])->name('establecimientos.show-opening')->where('id', '[0-9]+');
+        Route::post('/{id}/horarios', [ App\Http\Controllers\StoreController::class, 'saveOpening'])->name('establecimientos.save-opening');
+    });
 
     Route::resources([
         'establecimientos' => StoreController::class,
